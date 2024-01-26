5eb363a9ec4c31e8d0f00ab65bfae755477a3561
JeongGyu Kim(zzqyu) <wjdrb0626@gmail.com>
2024-01-26 23:25:20 +0900

    add files for setup
    Signed-off-by: JeongGyu Kim(zzqyu) <wjdrb0626@gmail.com>

---
+++ setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup, find_packages
+
+# 패키지 이름
+name = "django-tagify-widget"
+
+# 패키지 버전
+version = "0.1.0"
+
+# 패키지 설명
+description = "Django widgets with tagify.js"
+
+# 패키지 URL (옵션)
+url = "https://github.com/zzqyu/django-tagify-widget"
+
+# 저자 정보
+author = "zzqyu"
+author_email = "wjdrb0626@naver.com"
+
+# 패키지 라이센스
+license = "MIT"
+
+# 패키지 종속성 (다른 패키지에 대한 정보, 필요한 경우 추가)
+install_requires = [
+    'Django>=2.0.0',
+]
+
+# 패키지 정보
+setup(
+    name=name,
+    version=version,
+    description=description,
+    url=url,
+    author=author,
+    author_email=author_email,
+    license=license,
+    packages=find_packages(include=['tagify_widget']),
+    install_requires=install_requires,
+    python_requires='>=3',
+    platforms=['Any'],
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries",
+        "Framework :: Django",
+        "Environment :: Web Environment",
+    ],
+)
